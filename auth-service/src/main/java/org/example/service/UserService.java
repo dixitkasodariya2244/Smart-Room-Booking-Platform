90e92eefe4e94170b24c5616a54ce36a6edac73b
dixitkasodariya2244 <dixitkasodariya2244@gmail.com>
2025-10-10 22:22:37 +0100

    adding user service in auth-service

---
+++ auth-service/src/main/java/org/example/service/UserService.java
@@ -0,0 +1,49 @@
+package org.example.service;
+
+import org.example.dto.RegisterRequest;
+import org.example.dto.UserDTO;
+import org.example.model.User;
+import org.example.repository.UserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+    public UserService(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    public UserDTO register(RegisterRequest request){
+        if (userRepository.existsByUsername(request.getUsername())){
+            throw  new IllegalArgumentException("Username already exists!!");
+        }
+        if (userRepository.existsByEmail(request.getEmail())){
+            throw new IllegalArgumentException("Email already exists!!");
+        }
+        User user= User.builder()
+                .username(request.getUsername())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .build();
+        user.getRoles().add("ROLE_USER");
+
+        User saved = userRepository.save(user);
+
+        return new UserDTO(saved.getId(),saved.getUsername(), saved.getEmail());
+    }
+
+    public List<UserDTO> getAllUsers() {
+        return userRepository.findAll()
+                .stream()
+                .map(u -> new UserDTO(u.getId(), u.getUsername(), u.getEmail()))
+                .collect(Collectors.toList());
+    }
+
+}
+
