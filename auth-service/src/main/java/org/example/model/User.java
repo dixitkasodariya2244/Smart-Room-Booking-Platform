7a039a54712a416cddc5bf070e1f0479be7bfd50
dixitkasodariya2244 <dixitkasodariya2244@gmail.com>
2025-10-10 21:13:31 +0100

    adding user model in auth-service

---
+++ auth-service/src/main/java/org/example/model/User.java
@@ -0,0 +1,41 @@
+package org.example.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Builder
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique=true, nullable=false)
+    @NotBlank
+    private String username;
+
+    @Column(unique=true, nullable=false)
+    @Email
+    @NotBlank
+    private String email;
+
+    @Column(nullable=false)
+    @NotBlank
+    private String password; // stored hashed
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"))
+    @Column(name = "role")
+    private Set<String> roles = new HashSet<>();
+
+}
