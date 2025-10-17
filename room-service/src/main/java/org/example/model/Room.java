a1f40c3ab622c6e41965d264b8a5c39ce0c5e36b
Dixit Jenkins <dixitkasodariya2244@gmail.com>
2025-10-17 17:06:53 +0100

    feature/room-service: adding model

---
+++ room-service/src/main/java/org/example/model/Room.java
@@ -0,0 +1,26 @@
+package org.example.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "rooms")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Room {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String location;
+    private Integer capacity;
+    private String equipment;
+}
